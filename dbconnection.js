d8be08c8f097dca71251678048859fd1420e61ad
Erik Mayrhofer <obyoxar@gmail.com>
2018-03-22 21:37:14 +0100

    Added Sequelize reconnect

--- dbconnection.js
+++ dbconnection.js
@@ -1,14 +1,28 @@
 const Sequelize = require('sequelize');
+
+
 const sequelize = new Sequelize('apitest',
   process.env.PRB_DATABASE_USER,
   process.env.PRB_DATABASE_PASSPHRASE, {
     host: process.env.PRB_DATABASE_HOST,
     dialect: 'mysql',
 });
-sequelize.authenticate()
-.then(() => {
-  console.log('Connection has been established!');
-}).catch(() => {
-  console.log('Unable to connect to database:', err);
-});
+
+/** */
+function connect() {
+  console.log('Trying to connect to '+
+    process.env.PRB_DATABASE_HOST+' as '+process.env.PRB_DATABASE_USER);
+  sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established!');
+  }).catch((err) => {
+    console.log('Connection failed: '+err);
+    console.log('Trying to reconnect in 5s');
+    setTimeout(()=>{
+      connect();
+    }, 5000);
+  });
+}
+connect();
+
 module.exports = sequelize;
